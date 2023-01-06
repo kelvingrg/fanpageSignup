69862b54d88b2d47bbc3a3d45cdb1fd0b518a84a
kelvin george <kelvinpktl@gmail.com>
2023-01-06 09:54:07 +0530

    host

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,8 @@ var cors = require('cors')
 var app = express();
 require('dotenv').config(); 
 
-app.use(cors())
+app.use(cors([
+  'http://localhost:3000','https://fanpage-signup.onrender.com']))
 var usersRouter = require('./routes/users');
 
 
